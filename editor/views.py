5166ea78495dc8a11d0a301c4221131f8559641e
dimax <dimax.devel@gmail.com>
2020-10-17 14:39:14 +0000

    implemente redirect test

--- editor/views.py
+++ editor/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from editor import app
+from flask import redirect, url_for
 
 @app.route('/')
 def index():
-    return 'hello, world'
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return 'hello login'
