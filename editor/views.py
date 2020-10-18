6caaa18433f943cc12db27fdce1273c328e3d2fd
dimax <dimax.devel@gmail.com>
2020-10-18 13:08:54 +0000

    for debug3

--- editor/views.py
+++ editor/views.py
@@ -5,7 +5,7 @@ from flask import redirect, url_for, render_template
 from flask_github import GitHub
 github = GitHub(app)
 print(os.environ['GITHUB_CLIENT_ID'])
-app.config['GITHUB_CLIENT_ID'] = os.environ['GITHUB_CLIENT_ID']
+app.config['GITHUB_CLIENT_ID'] = d5b97a60a4380620b73c
 app.config['GIHTUB_CLIENT_SECRET'] = os.environ['GITHUB_CLIENT_SECRET']
 
 @app.route('/')
