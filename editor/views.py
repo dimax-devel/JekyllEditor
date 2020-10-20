ba75456ab865f68fcc55efdcba2246e42152ca22
dimax <dimax.devel@gmail.com>
2020-10-20 12:30:19 +0000

    import request

--- editor/views.py
+++ editor/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 from editor import app
-from flask import redirect, url_for, render_template, session
+from flask import redirect, url_for, render_template, session, request
 from flask_github import GitHub
 app.config['GITHUB_CLIENT_ID'] = os.environ['GITHUB_CLIENT_ID']
 app.config['GITHUB_CLIENT_SECRET'] = os.environ['GITHUB_CLIENT_SECRET']
