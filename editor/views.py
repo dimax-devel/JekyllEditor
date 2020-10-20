e6c771d37afc0bcc4927afcd2ebbe738dc15dc70
dimax <dimax.devel@gmail.com>
2020-10-20 14:50:27 +0000

    for debug

--- editor/views.py
+++ editor/views.py
@@ -39,6 +39,7 @@ def post():
     categories = str(input['categories'])
     post_contents = str(input['post-contents'])
     ref_result = github.raw_request('GET', 'https://api.github.com/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), access_token=token)
+    print(ref_result.status_code)
     return str(ref_result.status_code)
 #    ref_object_sha = ref_result.json()['object']['sha']
 #    commit_json = github.raw_request('GET', 'https://api.github.com/repos/{0}/{1}/git/commits/{2}'.format(uname, repo, ref_object_sha), access_token=token).json()
