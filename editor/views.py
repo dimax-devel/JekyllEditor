73866f59e59dd9bce563da98027b25ab4c455752
dimax <dimax.devel@gmail.com>
2020-10-23 14:45:41 +0000

    change file name

--- editor/views.py
+++ editor/views.py
@@ -48,7 +48,7 @@ def post():
     commit_tree_sha = commit_json['tree']['sha']
     blob_result = http_request('POST', '/repos/{0}/{1}/git/blobs'.format(uname, repo), token, {'content':'---\nlayout: post\ntitle "{0}"\ndate: {1:%Y/%m/%d %H:%M:%S} +0900\ncategories: {2}\n---\n\n{3}'.format(title, now, categories, post_contents)})
     blob_sha = blob_result['sha']
-    tree_sha = http_request('POST', '/repos/{0}/{1}/git/trees'.format(uname, repo), token, {'base_tree':commit_tree_sha, 'tree':[{'path':'_posts/{0:%Y%m%d%H%M%S}.md'.format(now), 'mode':'100644', 'type':'blob', 'sha':blob_sha}]})['sha']
+    tree_sha = http_request('POST', '/repos/{0}/{1}/git/trees'.format(uname, repo), token, {'base_tree':commit_tree_sha, 'tree':[{'path':'_posts/{0:%Y-%m-%d-%H%M%S}.md'.format(now), 'mode':'100644', 'type':'blob', 'sha':blob_sha}]})['sha']
     new_commit_sha = http_request('POST', '/repos/{0}/{1}/git/commits'.format(uname, repo), token, {'message':'new post:{0:%Y/%m/%d %H:%M:%S}'.format(now), 'parents':[commit_sha], 'tree':tree_sha})['sha']
     res = http_request('PATCH', '/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), token, {'sha':new_commit_sha})
     return redirect(url_for('posted'))
