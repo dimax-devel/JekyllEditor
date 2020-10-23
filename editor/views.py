dd3f25de1c3445f3eed33ebd1f29a51b774ecb26
dimax <dimax.devel@gmail.com>
2020-10-23 11:12:39 +0000

    for debug

--- editor/views.py
+++ editor/views.py
@@ -46,12 +46,14 @@ def post():
     print(post_contents)
     ref_result = github.raw_request('GET', 'https://api.github.com/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), access_token=token)
     print(ref_result.status_code)
-#    return str(ref_result.status_code)
     ref_object_sha = ref_result.json()['object']['sha']
     commit_json = github.raw_request('GET', 'https://api.github.com/repos/{0}/{1}/git/commits/{2}'.format(uname, repo, ref_object_sha), access_token=token).json()
     commit_sha = commit_json['sha']
     commit_tree_sha = commit_json['tree']['sha']
-    blob_sha = github.raw_request('POST', 'https://api.github.com/repos/{0}/{1}/git/blobs'.format(uname, repo), access_token=token, headers={'content':post_contents}).json()['sha']
+    blob_result = github.raw_request('POST', 'https://api.github.com/repos/{0}/{1}/git/blobs'.format(uname, repo), access_token=token, headers={'content':post_contents})
+    print(blob_result.status_code)
+    print(blob_result.json())
+    blob_sha = blob_result.json()['sha']
     now = datetime.now()
     tree_sha = github.raw_request('POST', 'https://api.github.com/repos/{0}/{1}/git/trees', access_token=token, headers={'base_tree':commit_tree_sha, 'tree':{'path':'_post/{0:%Y%m%d%H%M%S}.md'.format(now), 'mode':'100644', 'type':'blob', 'sha':blob_sha}}).json()['sha']
     new_commit_sha = github.raw_request('POST', 'https://api.github.com/repos/{0}/{1}/git/commits'.format(uname, repo), access_token=token, headers={'message':'new post:{0:%Y/%m/%d %H:%M:%S}'.format(now), 'parents':[commit_sha], 'tree':tree_sha}).json()['sha']
