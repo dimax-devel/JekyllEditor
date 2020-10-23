13d7b828a7f6b7fd927a5c34145ae15f07d189d3
dimax <dimax.devel@gmail.com>
2020-10-23 15:22:14 +0000

    revert return code

--- editor/views.py
+++ editor/views.py
@@ -46,10 +46,10 @@ def post():
     commit_json = http_request('GET', '/repos/{0}/{1}/git/commits/{2}'.format(uname, repo, ref_object_sha), token)
     commit_sha = commit_json['sha']
     commit_tree_sha = commit_json['tree']['sha']
-    blob_result = http_request('POST', '/repos/{0}/{1}/git/blobs'.format(uname, repo), token, {'content':'---\r\nlayout: post\r\ntitle "{0}"\r\ndate: {1:%Y/%m/%d %H:%M:%S} +0900\r\ncategories: {2}\r\n---\r\n\r\n{3}'.format(title, now, categories, post_contents)})
+    blob_result = http_request('POST', '/repos/{0}/{1}/git/blobs'.format(uname, repo), token, {'content':'---\nlayout: post\ntitle: "{0}"\ndate: {1:%Y/%m/%d %H:%M:%S} +0900\ncategories: {2}\n---\n\n{3}'.format(title, now, categories, post_contents)})
     blob_sha = blob_result['sha']
     tree_sha = http_request('POST', '/repos/{0}/{1}/git/trees'.format(uname, repo), token, {'base_tree':commit_tree_sha, 'tree':[{'path':'_posts/{0:%Y-%m-%d-%H%M%S}.md'.format(now), 'mode':'100644', 'type':'blob', 'sha':blob_sha}]})['sha']
-    new_commit_sha = http_request('POST', '/repos/{0}/{1}/git/commits'.format(uname, repo), token, {'message':'new post:{0:%Y/%m/%d %H:%M:%S}'.format(now), 'parents':[commit_sha], 'tree':tree_sha})['sha']
+    new_commit_sha = http_request('POST', '/repos/{0}/{1}/git/commits'.format(uname, repo), token, {'message':'new post: {0:%Y/%m/%d %H:%M:%S}'.format(now), 'parents':[commit_sha], 'tree':tree_sha})['sha']
     res = http_request('PATCH', '/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), token, {'sha':new_commit_sha})
     return redirect(url_for('posted'))
 
