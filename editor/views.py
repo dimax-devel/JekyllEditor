89af248ecb5cb52195b497c49e7dd81a04718fd6
dimax <dimax.devel@gmail.com>
2020-10-23 12:57:44 +0000

    change to use requests

--- editor/views.py
+++ editor/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import requests
 from editor import app
 from flask import redirect, url_for, render_template, session, request
 from flask_github import GitHub
@@ -39,25 +40,19 @@ def post():
     repo = str(input['select-repo'])
     print(repo)
     title = str(input['title'])
-    print(title)
     categories = str(input['categories'])
-    print(categories)
     post_contents = str(input['post-contents'])
-    print(post_contents)
-    ref_result = github.raw_request('GET', 'https://api.github.com/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), access_token=token)
-    print(ref_result.status_code)
-    ref_object_sha = ref_result.json()['object']['sha']
-    commit_json = github.raw_request('GET', 'https://api.github.com/repos/{0}/{1}/git/commits/{2}'.format(uname, repo, ref_object_sha), access_token=token).json()
+    ref_json = request('GET', '/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), token)
+    ref_object_sha = ref_json['object']['sha']
+    commit_json = request('GET', '/repos/{0}/{1}/git/commits/{2}'.format(uname, repo, ref_object_sha), token)
     commit_sha = commit_json['sha']
     commit_tree_sha = commit_json['tree']['sha']
-    blob_result = github.raw_request('POST', 'https://api.github.com/repos/{0}/{1}/git/blobs'.format(uname, repo), token, data={"content":post_contents})
-    print(blob_result.status_code)
-    print(blob_result.json())
-    blob_sha = blob_result.json()['sha']
+    blob_result = request('POST', '/repos/{0}/{1}/git/blobs'.format(uname, repo), token, {'content':post_contents})
+    blob_sha = blob_result['sha']
     now = datetime.now()
-    tree_sha = github.raw_request('POST', 'https://api.github.com/repos/{0}/{1}/git/trees', token, base_tree=commit_tree_sha, tree={'path':'_post/{0:%Y%m%d%H%M%S}.md'.format(now), 'mode':'100644', 'type':'blob', 'sha':blob_sha}).json()['sha']
-    new_commit_sha = github.raw_request('POST', 'https://api.github.com/repos/{0}/{1}/git/commits'.format(uname, repo), token, message={'new post:{0:%Y/%m/%d %H:%M:%S}'.format(now)}, parents=[commit_sha], tree=tree_sha).json()['sha']
-    status = github.raw_request('PATCH', 'https://api.github.com/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), token, sha=new_commit_sha).status_code
+    tree_sha = request('POST', '/repos/{0}/{1}/git/trees', token, {'base_tree':commit_tree_sha, 'tree':{'path':'_post/{0:%Y%m%d%H%M%S}.md'.format(now), 'mode':'100644', 'type':'blob', 'sha':blob_sha}})['sha']
+    new_commit_sha = request('POST', '/repos/{0}/{1}/git/commits'.format(uname, repo), token, {'message':'new post:{0:%Y/%m/%d %H:%M:%S}'.format(now), 'parents':[commit_sha], 'tree':tree_sha})['sha']
+    github.raw_request('PATCH', '/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), token, {'sha':new_commit_sha})
 #    return redirect(url_for('posted'), sts=status)
 #    return status
     return 'test'
@@ -65,3 +60,20 @@ def post():
 @app.route('/posted')
 def posted(sts):
     return render_template('posted.html', status=sts)
+
+def request(method, path, token, data=None):
+    url = 'https://api.github.com{0}'.format(path)
+    auth_header = 'Authorization: token {0}'.format(token)
+    if method == 'GET':
+        print('GET {0}', url)
+        res = requests.get(url, headers=auth_header)
+    else if method == 'POST':
+        print('POST {0}', url)
+        res = requests.post(url, headers=auth_header, json=data)
+    else if method == 'PATCH':
+        print('PATCH {0}', url)
+        res = requests.patch(url, headers=auth_header, json=data)
+    else:
+        return None   
+    print(res.status_code)
+    return res.json()
