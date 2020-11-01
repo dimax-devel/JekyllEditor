5a31ba0ef9a5f494c46d83456cc7e08d14416b27
dimax <dimax.devel@gmail.com>
2020-11-01 02:01:03 +0000

    add try catch

--- editor/views.py
+++ editor/views.py
@@ -33,29 +33,37 @@ def authorized(oath_token):
     
 @app.route('/post', methods = ['POST'])
 def post():
-    input = request.form
-    uname = str(input['user-name'])
-    token = str(input['auth-token'])
-    repo = str(input['select-repo'])
-    title = str(input['title'])
-    categories = str(input['categories'])
-    post_contents = str(input['post-contents'])
-    jst = timezone(timedelta(hours=+9), 'JST')
-    now = datetime.now(jst)
-    ref_object_sha = http_request('GET', '/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), token)['object']['sha']
-    commit_json = http_request('GET', '/repos/{0}/{1}/git/commits/{2}'.format(uname, repo, ref_object_sha), token)
-    commit_sha = commit_json['sha']
-    commit_tree_sha = commit_json['tree']['sha']
-    blob_result = http_request('POST', '/repos/{0}/{1}/git/blobs'.format(uname, repo), token, {'content':'---\nlayout: post\ntitle: "{0}"\ndate: {1:%Y/%m/%d %H:%M:%S} +0900\ncategories: {2}\n---\n\n{3}'.format(title, now, categories, post_contents)})
-    blob_sha = blob_result['sha']
-    tree_sha = http_request('POST', '/repos/{0}/{1}/git/trees'.format(uname, repo), token, {'base_tree':commit_tree_sha, 'tree':[{'path':'_posts/{0:%Y-%m-%d-%H%M%S}.md'.format(now), 'mode':'100644', 'type':'blob', 'sha':blob_sha}]})['sha']
-    new_commit_sha = http_request('POST', '/repos/{0}/{1}/git/commits'.format(uname, repo), token, {'message':'new post: {0:%Y/%m/%d %H:%M:%S}'.format(now), 'parents':[commit_sha], 'tree':tree_sha})['sha']
-    res = http_request('PATCH', '/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), token, {'sha':new_commit_sha})
-    return redirect(url_for('posted'))
+    try:
+        input = request.form
+        uname = str(input['user-name'])
+        token = str(input['auth-token'])
+        repo = str(input['select-repo'])
+        title = str(input['title'])
+        categories = str(input['categories'])
+        post_contents = str(input['post-contents'])
+        jst = timezone(timedelta(hours=+9), 'JST')
+        now = datetime.now(jst)
+        ref_object_sha = http_request('GET', '/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), token)['object']['sha']
+        commit_json = http_request('GET', '/repos/{0}/{1}/git/commits/{2}'.format(uname, repo, ref_object_sha), token)
+        commit_sha = commit_json['sha']
+        commit_tree_sha = commit_json['tree']['sha']
+        blob_result = http_request('POST', '/repos/{0}/{1}/git/blobs'.format(uname, repo), token, {'content':'---\nlayout: post\ntitle: "{0}"\ndate: {1:%Y/%m/%d %H:%M:%S} +0900\ncategories: {2}\n---\n\n{3}'.format(title, now, categories, post_contents)})
+        blob_sha = blob_result['sha']
+        tree_sha = http_request('POST', '/repos/{0}/{1}/git/trees'.format(uname, repo), token, {'base_tree':commit_tree_sha, 'tree':[{'path':'_posts/{0:%Y-%m-%d-%H%M%S}.md'.format(now), 'mode':'100644', 'type':'blob', 'sha':blob_sha}]})['sha']
+        new_commit_sha = http_request('POST', '/repos/{0}/{1}/git/commits'.format(uname, repo), token, {'message':'new post: {0:%Y/%m/%d %H:%M:%S}'.format(now), 'parents':[commit_sha], 'tree':tree_sha})['sha']
+        res = http_request('PATCH', '/repos/{0}/{1}/git/refs/heads/master'.format(uname, repo), token, {'sha':new_commit_sha})
+        return redirect(url_for('posted'))
+    except KeyError as instance:
+        print instance
+        return redirect(url_for('error'))
 
 @app.route('/posted')
 def posted():
-    return render_template('posted.html')
+    return render_template('posted.html', message='Posted!')
+
+@app.route('/error')
+def error():
+    return render_template('posted.html', message='Failed!')
 
 def http_request(method, path, token, data=None):
     url = 'https://api.github.com{0}'.format(path)
